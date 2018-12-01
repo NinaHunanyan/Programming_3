1875dbafb6cf9a0e06d15249da6b42d645d322e6
Your Name <you@example.com>
2018-12-01 15:34:22 +0400

    arancnacrel em class.js-i class-ner

---
+++ public/class.mard.js
@@ -0,0 +1,117 @@
+class Mard {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.energy = 50;
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+            [this.x - 2, this.y - 2],
+            [this.x - 1, this.y - 2],
+            [this.x, this.y + 2],
+            [this.x + 1, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x + 2, this.y - 1],
+            [this.x + 2, this.y],
+            [this.x + 2, this.y + 1],
+            [this.x + 2, this.y + 2],
+            [this.x + 1, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x - 1, this.y + 2],
+            [this.x - 2, this.y + 2],
+            [this.x - 2, this.y + 1],
+            [this.x - 2, this.y],
+            [this.x - 2, this.y - 1]
+        ];
+    }
+
+    chooseCell(num) {
+        this.getNewCoordinates();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == num) {
+                    found.push(this.directions[i]);
+                }
+                else if (matrix[y][x].index == num) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+    move() {
+        if (this.acted == false) {
+            var newCell = random(this.chooseCell(0));
+
+            if (newCell) {
+                var newX = newCell[0];
+                var newY = newCell[1];
+
+                matrix[newY][newX] = matrix[this.y][this.x];
+                matrix[this.y][this.x] = 0;
+                this.x = newX;
+                this.y = newY;
+            }
+            this.energy -= 2;
+            if (this.energy <= 0) {
+                this.die();
+            }
+            this.acted = true;
+        }
+
+    }
+    eat() {
+        if (this.acted == false) {
+            var newCell = random(this.chooseCell(random([2, 3])));
+            if (newCell) {
+                var newX = newCell[0];
+                var newY = newCell[1];
+
+                matrix[newY][newX] = matrix[this.y][this.x];
+                matrix[this.y][this.x] = 0;
+                this.x = newX;
+                this.y = newY;
+                this.energy += 3;
+                if (this.energy >= 80) {
+                    this.mul();
+                    this.energy = 50;
+                }
+
+                this.acted = true;
+            }
+            else {
+                this.move();
+            }
+        }
+    }
+    mul() {
+
+        var newCell = random(this.chooseCell(0));
+
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[newY][newX] = new Mard(newX, newY, 4);
+
+        }
+
+    }
+    die() {
+
+        matrix[this.y][this.x] = 0;
+
+    }
+}
---
+++ public/class.vampir.js
@@ -0,0 +1,150 @@
+class Vampir {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.energy = 16;
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+            [this.x - 2, this.y - 2],
+            [this.x - 1, this.y - 2],
+            [this.x, this.y + 2],
+            [this.x + 1, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x + 2, this.y - 1],
+            [this.x + 2, this.y],
+            [this.x + 2, this.y + 1],
+            [this.x + 2, this.y + 2],
+            [this.x + 1, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x - 1, this.y + 2],
+            [this.x - 2, this.y + 2],
+            [this.x - 2, this.y + 1],
+            [this.x - 2, this.y],
+            [this.x - 2, this.y - 1],
+            [this.x - 3, this.y - 3],
+            [this.x - 3, this.y - 2],
+            [this.x - 3, this.y - 1],
+            [this.x - 3, this.y],
+            [this.x - 3, this.y + 1],
+            [this.x - 3, this.y + 2],
+            [this.x - 3, this.y + 3],
+            [this.x - 2, this.y - 3],
+            [this.x - 2, this.y + 3],
+            [this.x - 1, this.y - 3],
+            [this.x - 1, this.y + 3],
+            [this.x, this.y - 3],
+            [this.x, this.y + 3],
+            [this.x + 1, this.y - 3],
+            [this.x + 1, this.y + 3],
+            [this.x + 2, this.y - 3],
+            [this.x + 2, this.y - 3],
+            [this.x + 3, this.y - 3],
+            [this.x + 3, this.y + 3],
+            [this.x + 2, this.y - 2],
+            [this.x + 2, this.y - 1],
+            [this.x + 2, this.y],
+            [this.x + 2, this.y + 1],
+            [this.x + 2, this.y + 2]
+        ];
+    }
+
+    chooseCell(num) {
+        this.getNewCoordinates();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == num) {
+                    found.push(this.directions[i]);
+                }
+                else if (matrix[y][x].index == num) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+    move() {
+        if (this.acted == false) {
+            var newCell = random(this.chooseCell(0));
+
+            if (newCell) {
+                var newX = newCell[0];
+                var newY = newCell[1];
+
+                matrix[newY][newX] = matrix[this.y][this.x];
+                matrix[this.y][this.x] = 0;
+                this.x = newX;
+                this.y = newY;
+            }
+            this.energy -= 2;
+            if (this.energy <= 0) {
+                this.die();
+            }
+            this.acted = true;
+        }
+
+    }
+    eat() {
+        if (this.acted == false) {
+            var newCell = random(this.chooseCell(4));
+            if (newCell) {
+                var newX = newCell[0];
+                var newY = newCell[1];
+                console.log("Darav mard");
+                matrix[newY][newX] = new Vampir(newX, newY, 5);
+            }
+            else {
+                newCell = random(this.chooseCell(random([3, 2])));
+                if (newCell) {
+                    var newX = newCell[0];
+                    var newY = newCell[1];
+
+                    matrix[newY][newX] = matrix[this.y][this.x];
+                    matrix[this.y][this.x] = 0;
+                    this.x = newX;
+                    this.y = newY;
+                    this.energy += 3;
+                    if (this.energy >= 100) {
+                        this.mul();
+                        this.energy = 16;
+                    }
+
+                    this.acted = true;
+                }
+                else {
+                    this.move();
+                }
+            }
+        }
+    }
+    mul() {
+
+        var newCell = random(this.chooseCell(0));
+
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[newY][newX] = new Vampir(newX, newY, 5);
+
+        }
+
+    }
+    die() {
+
+        matrix[this.y][this.x] = 0;
+
+    }
+}
